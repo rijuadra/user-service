dce6a320bfd9265e6121dd9aa6c06ca52d2bb736
Saikat Das <rijuadra@gmail.com>
2021-01-15 14:37:33 +0530

    docker related configuration added

--- src/main/java/com/bank/config/KafkaConfiguration.java
+++ src/main/java/com/bank/config/KafkaConfiguration.java
@@ -27,7 +27,7 @@ public class KafkaConfiguration {
         deserializer.setRemoveTypeHeaders(false);
         deserializer.addTrustedPackages("*");
         deserializer.setUseTypeMapperForKey(true);
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"172.24.176.1:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG,"group_id");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
@@ -46,7 +46,7 @@ public class KafkaConfiguration {
         deserializer.setRemoveTypeHeaders(false);
         deserializer.addTrustedPackages("*");
         deserializer.setUseTypeMapperForKey(true);
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"172.24.176.1:9092");
         config.put(ConsumerConfig.GROUP_ID_CONFIG,"group_id");
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
@@ -61,7 +61,7 @@ public class KafkaConfiguration {
     @Bean
     public ProducerFactory<String, User> producerFactory(){
         Map<String, Object> config=new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"172.24.176.1:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory(config);
